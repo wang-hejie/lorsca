8b5090f77f850d5e7407d467d4766a6e965b3031
Wang Hejie <984468110@qq.com>
2020-10-08 10:49:33 +0800

    start first experience with 4 species

--- first_round_experience.py
+++ first_round_experience.py
@@ -8,12 +8,9 @@ script_path = os.path.abspath(__file__)  # 软件根目录/self-correction.py
 software_path = os.path.abspath(os.path.dirname(script_path) + os.path.sep + ".")  # 软件根目录
 lorsca_path = software_path + '/lorsca.py'
 
-# species_list = ['ecoli', 'scere']
-species_list = ['scere']
-# folds_list = ['10', '30', '50', '75', '100']
-folds_list = ['30']
+species_list = ['ecoli', 'scere', 'dmela', 'athal']
+folds_list = ['10', '30', '50', '75', '100']
 tools_list = ['raw', 'mecat2', 'falcon', 'lorma', 'canu', 'pbcr','flas', 'consent', 'daccord', 'sprai', 'pbdagcon']
-# company_list = ['pacbio', 'ont']
 company_list = ['pacbio']
 assembler_list = ['miniasm']
 
